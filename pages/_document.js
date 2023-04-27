72d395cea66fc8614a7ab2ea71c43177598c7f58
jonathan henrique <henriquejonathan500@gmail.com>
2023-04-27 11:45:30 -0300

    add photo

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-        <Head >
+        <Head>
+            <title>Rental-boat</title>
+            <meta property="og:image" content="https://jonathanhnr.github.io/Rental-boat/uma-lancha-veiculos-barcos-1499227.jpg" />
             <script src="https://kit.fontawesome.com/a388be2306.js" crossOrigin="anonymous" defer/>
         </Head>
       <body>
