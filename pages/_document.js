6ba6ddbb2583986e1136e512f422e712be7294e9
jonathan henrique <henriquejonathan500@gmail.com>
2023-04-27 12:34:44 -0300

    add meta tags image

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,9 @@ export default function Document() {
   return (
     <Html lang="en">
         <Head>
-            <link rel="icon" type="image/jpg" href="/Rental-boat/uma-lancha-veiculos-barcos-1499227.7feb3408.jpg" />
+            <link rel="icon" type="image/jpg" href="/Rental-boat/uma-lancha-veiculos-barcos-1499227.jpg" />
             <title>Rental-boat</title>
+            <meta property="og:url" content="https://jonathanhnr.github.io/Rental-boat/"/>
             <meta property="og:image" content="https://jonathanhnr.github.io/Rental-boat/rental-boat.png" />
             <script src="https://kit.fontawesome.com/a388be2306.js" crossOrigin="anonymous" defer/>
         </Head>
