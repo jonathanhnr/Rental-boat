2c7d089fba7920a85247e24512338455b8fd227d
jonathan henrique <henriquejonathan500@gmail.com>
2023-04-27 13:25:49 -0300

    added og image tag

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/styles.scss"
 import NavBar from "../components/NavBar/NavBar";
 import Layout from "../components/Layout/Layout";
 
+
 export default function App({ Component, pageProps }) {
   return (
      <Layout>
