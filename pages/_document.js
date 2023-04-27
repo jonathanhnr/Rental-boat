dc9da60497f36920309bc3a15db7a22eb041f2fe
jonathan henrique <henriquejonathan500@gmail.com>
2023-04-27 13:13:09 -0300

    corrigindo preview

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,8 @@ export default function Document() {
         <Head>
             <link rel="icon" type="image/jpg" href="/Rental-boat/_next/static/media/uma-lancha-veiculos-barcos-1499227.7feb3408.jpg" />
             <title>Rental-boat</title>
-            <meta property="og:url" content="https://jonathanhnr.github.io/Rental-boat/"/>
-            <meta property="og:image" content="https://jonathanhnr.github.io/Rental-boat/_next/static/media/rental-boat.png" />
+            <meta property="og:url" content="https://jonathanhnr.github.io/Rental-boat/rental-boat.png"/>
+            <meta property="og:image" content={"/Rental-boat/rental-boat.png"}/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <script src="https://kit.fontawesome.com/a388be2306.js" crossOrigin="anonymous" defer/>
         </Head>
