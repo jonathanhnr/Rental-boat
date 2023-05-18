48bbef1d124e477d8feb66e40e270eeb1efa3bb4
jonathan henrique <henriquejonathan500@gmail.com>
2023-05-18 16:25:05 -0300

    update footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -14,39 +14,27 @@ function Footer() {
                         </li>
                         <li>
                             <a href="tel:123456789">
-                                <i className="fa-solid fa-phone"></i> &nbsp; (123) -456-789
+                                <i className="fa-solid fa-phone"></i> &nbsp;
                             </a>
                         </li>
 
                         <li>
-                            <a
-                                href="mailto:
-                carrental@gmail.com"
-                            >
+                            <a>
                                 <i className="fa-solid fa-envelope"></i>
-                                &nbsp; Teste@gmail.com
+                                &nbsp;
                             </a>
                         </li>
                     </ul>
 
                     <ul className="footer-content__2">
                         <li>EMPRESA</li>
-
-                        <li>
-                            <Link href="/">New York</Link>
-                        </li>
-                        <li>
-                            <Link href="/">Careers</Link>
-                        </li>
                         <li>
                             <Link href="/">Mobile</Link>
                         </li>
                         <li>
                             <Link href="/">Blog</Link>
                         </li>
-                        <li>
-                            <Link href="/">How we work</Link>
-                        </li>
+
                     </ul>
 
                     <ul className="footer-content__2">
@@ -59,10 +47,10 @@ function Footer() {
                     <ul className="footer-content__2">
                         <li>INSCRIÇÃO</li>
                         <li>
-                            <p>subscreva o seu endereço de e-mail para as últimas notícias e atualizações.</p>
+                            <p>Escreva o seu endereço de e-mail para as últimas notícias e atualizações.</p>
                         </li>
                         <li>
-                            <input type="email" placeholder="Enter Email Address"></input>
+                            <input type="email" placeholder="Email"></input>
                         </li>
                         <li>
                             <button className="submit-email">Enviar</button>
